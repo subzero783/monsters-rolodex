bda8b432abd6c51d800a3af138d154fdb682f8c5
Gus <gusta1296@gmail.com>
2019-08-16 15:13:05 -0700

    deploying

---
+++ build/precache-manifest.96fb0d66ddd3488a731aee1a4df0aceb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "264b15d6b982b82d575199bbff6a6c55",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "63e68621bff43bc8bd08",
+    "url": "/monsters-rolodex/static/css/main.7d27e270.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "63e68621bff43bc8bd08",
+    "url": "/monsters-rolodex/static/js/main.0e1ae40c.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
